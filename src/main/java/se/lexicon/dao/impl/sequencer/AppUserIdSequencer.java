eb08fed4c5609376e525e2424c2e81a771896d78
MariusAdrianStoica <stoichitza@yahoo.com>
2022-11-27 19:07:31 +0100

    implement all daoImpl classes with Singleton design pattern

---
+++ src/main/java/se/lexicon/dao/impl/sequencer/AppUserIdSequencer.java
@@ -0,0 +1,15 @@
+package se.lexicon.dao.impl.sequencer;
+
+public class AppUserIdSequencer {
+
+    private static int sequencer =100;
+
+    public static int nextId(){
+        return ++sequencer;
+
+    }
+
+
+
+
+}
